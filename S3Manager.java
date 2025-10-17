24a0f1c3a27641f51aa04e280b59a178f1ec663f
Kamal P <kamaldhanam888@gmail.com>
2025-10-17 20:33:50 +0530

    Create S3Manager.java

---
+++ S3Manager.java
@@ -0,0 +1,33 @@
+package p2p;
+
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.File;
+
+public class S3Manager {
+    private static final String BUCKET_NAME = "your-s3-bucket-name";
+
+    public static void uploadDocument(String filePath, String userEmail) {
+        try {
+            S3Client s3 = S3Client.builder()
+                    .region(Region.US_EAST_1)
+                    .credentialsProvider(ProfileCredentialsProvider.create())
+                    .build();
+
+            File file = new File(filePath);
+
+            PutObjectRequest request = PutObjectRequest.builder()
+                    .bucket(BUCKET_NAME)
+                    .key(userEmail + "/" + file.getName())
+                    .build();
+
+            s3.putObject(request, file.toPath());
+            System.out.println("Uploaded document to S3: " + file.getName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
